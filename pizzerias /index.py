8061cd7fc198a2015d22ce89958698d28bf668f8
Hassan Azimi <hassanazimi@users.noreply.github.com>
2020-04-30 21:35:12 +0100

    add everything

--- pizzerias /index.py
+++ pizzerias /index.py
@@ -19,44 +19,44 @@ def load(file_name):
 
 
 class Block:
-    def __init__(self, dim, location, capacity, debug=True):
+    def __init__(self, dimention, location, capacity, debug=True):
         if (capacity < 1) | (capacity > 100):
             raise Exception('Range must be between 1 to 100')
 
-        if (location[0] < 1) | (location[1] > dim):
-            raise Exception('Location must be within {} x {}'.format(dim, dim))
+        if (location[0] < 1) | (location[1] > dimention):
+            raise Exception('Location must be within {} x {}'.format(dimention, dimention))
 
-        if (dim < 1) | (dim > 1000):
+        if (dimention < 1) | (dimention > 1000):
             raise Exception('Dimensions is not valid!')
 
         self.debug = debug
-        self.dim = dim
+        self.dimention = dimention
         self.location = location
         self.capacity = capacity
-        self.block = numpy.zeros((dim, dim))
+        self.block = numpy.zeros((dimention, dimention))
 
     def build(self):
         x = self.location[0] - 1
         y = self.location[1] - 1
 
         for i in range(self.capacity + 1):
-            if (x + i) < dim:
+            if (x + i) < dimention:
                 self.block[x + i][y] = 1
             if (x - i) >= 0:
                 self.block[x - i][y] = 1
-            if (y + i) < dim:
+            if (y + i) < dimention:
                 self.block[x][y + i] = 1
             if (y - i) >= 0:
                 self.block[x][y - i] = 1
 
         for i in range(1, self.capacity):
-            if (x + i < dim) & (y + i < dim):
+            if (x + i < dimention) & (y + i < dimention):
                 self.block[x + i][y + i] = 1
             if (x - i >= 0) & (y - i >= 0):
                 self.block[x - i][y - i] = 1
-            if (x + i < dim) & (y - i >= 0):
+            if (x + i < dimention) & (y - i >= 0):
                 self.block[x + i][y - i] = 1
-            if (x - i >= 0) & (y + i < dim):
+            if (x - i >= 0) & (y + i < dimention):
                 self.block[x - i][y + i] = 1
 
         self.block = numpy.flipud(self.block)
@@ -70,11 +70,11 @@ class Block:
 if __name__ == '__main__':
     file_name = 'input.txt'
 
-    dim, num_pizzerias, pizzerias = load(file_name)
+    dimention, num_pizzerias, pizzerias = load(file_name)
 
-    sum_range = numpy.zeros((dim, dim))
+    sum_range = numpy.zeros((dimention, dimention))
     for pizzeria in pizzerias:
-        block = Block(dim, pizzeria[0:-1], pizzeria[-1], debug=False)
+        block = Block(dimention, pizzeria[0:-1], pizzeria[-1], debug=False)
         pizza_range = block.build()
         sum_range += pizza_range
 
